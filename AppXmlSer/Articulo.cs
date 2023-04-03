80ad12a4c437b8253f891c4edd380d02b4ec812a
DIEGO <DIEGO@DESKTOP-F4MTD1E>
2023-04-02 21:09:51 -0600

    Agregar archivos de proyecto.

---
+++ AppXmlSer/Articulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppXmlSer
+{
+    public class Articulo
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ AppXmlSer/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace AppXmlSer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SerializeDataSet("datos.xml");
+            Process.Start("datos.xml");
+        }
+        private void serialzeArtiuclo(string filename)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(Articulo));
+            // Creates a DataSet; adds a table, column, and ten rows.
+            DataSet ds = new DataSet("myDataSet");
+            DataTable t = new DataTable("table1");
+            DataColumn c = new DataColumn("thing");
+            t.Columns.Add(c);
+            ds.Tables.Add(t);
+            DataRow r;
+            for (int i = 0; i < 10; i++)
+            {
+                r = t.NewRow();
+                r[0] = "Thing " + i;
+                t.Rows.Add(r);
+            }
+            TextWriter writer = new StreamWriter(filename);
+            ser.Serialize(writer, ds);
+            writer.Close();
+        }
+        private void SerializeDataSet(string filename)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(DataSet));
+            // Creates a DataSet; adds a table, column, and ten rows.
+            DataSet ds = new DataSet("myDataSet");
+            DataTable t = new DataTable("table1");
+            DataColumn c = new DataColumn("thing");
+            t.Columns.Add(c);
+            ds.Tables.Add(t);
+            DataRow r;
+            for (int i = 0; i < 10; i++)
+            {
+                r = t.NewRow();
+                r[0] = "Thing " + i;
+                t.Rows.Add(r);
+            }
+            TextWriter writer = new StreamWriter(filename);
+            ser.Serialize(writer, ds);
+            writer.Close();
+        }
+    }
+}
